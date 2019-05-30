4f0960440a4c11eb94241f962b9eb6fbd3ad6b8d
Nanbu0224 <40815063+Nanbu0224@users.noreply.github.com>
2019-05-31 00:39:40 +0800

    Add files via upload

--- main/main.js
+++ main/main.js
@@ -1,4 +1,12 @@
 // Write your cade below:
-module.exports = function main() {
-  
+module.exports = function main(first,second) {
+  /*if (first=='9' && second=='3')
+  {
+	  return 0;
+  }
+  else if(first=='11'&&second=='4')
+  {
+	  return 3;
+  }*/
+  return first%second;
 };
